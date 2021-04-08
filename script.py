dfe3d19db2ee14439af0a7786aeac250708a9ce8
Sina K <15523694+sinakokab@users.noreply.github.com>
2021-04-08 05:23:12 +0100

    Update script.py
    updated for better automatic keyframe tracking when input to ebsynth

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-import os
+import os, math
 from pathlib import Path
 
 user_directory = input("Input your desired directory.\n")
@@ -12,6 +12,12 @@ else:
 
 DeletedFileCount = 0
 RenamedFileCount = 0
+RenamedFileCountIndex = ""
+
+totalfilecount = 0
+for f in os.listdir():
+    totalfilecount+= 1
+
 for f in os.listdir():
     file_name, file_ext = os.path.splitext(f)
     new_path = os.path.join(user_directory, f)
@@ -22,7 +28,16 @@ for f in os.listdir():
         os.remove(new_path);
         print("Deleted " + str(DeletedFileCount) + " files.")
     else:
-        renamed_file_path = os.path.join(user_directory, str(RenamedFileCount) + file_ext)
+        if (len(str(totalfilecount)) > len(str(RenamedFileCount))):
+            DigitDifference = (len(str(totalfilecount)) - len(str(RenamedFileCount)))
+            print("RenamedFileCount: " + str(RenamedFileCount) +"\nTotalFileCount: " + str(totalfilecount))
+            print("Digit Difference " + str(DigitDifference))
+            for i in range(0, DigitDifference):
+                RenamedFileCountIndex += "0"
+        RenamedFileCountIndex += str(RenamedFileCount)
+
+        renamed_file_path = os.path.join(user_directory, "pic" + RenamedFileCountIndex + file_ext)
         os.rename(new_path, renamed_file_path)
         print("Renamed:\n" + new_path + "\nto:\n" + renamed_file_path)
         RenamedFileCount += 1
+        RenamedFileCountIndex = ""
